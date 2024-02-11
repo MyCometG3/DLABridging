a6a6e98acc52a4af1bd4ce2bf067b6df817c4ef3
Takashi Mochizuki <mycometg3@icloud.com>
2024-02-11 20:57:13 +0900

    It's 2024 now.

--- Source/C++ Class/DLABNotificationCallback.mm
+++ Source/C++ Class/DLABNotificationCallback.mm
@@ -3,7 +3,7 @@
 //  DLABridging
 //
 //  Created by Takashi Mochizuki on 2017/08/26.
-//  Copyright © 2017-2023 MyCometG3. All rights reserved.
+//  Copyright © 2017-2024 MyCometG3. All rights reserved.
 //
 
 /* This software is released under the MIT License, see LICENSE.txt. */
--- Source/C++ Class/DLABProfileCallback.h
+++ Source/C++ Class/DLABProfileCallback.h
@@ -3,7 +3,7 @@
 //  DLABridging
 //
 //  Created by Takashi Mochizuki on 2020/03/13.
-//  Copyright © 2020-2023 MyCometG3. All rights reserved.
+//  Copyright © 2020-2024 MyCometG3. All rights reserved.
 //
 
 /* This software is released under the MIT License, see LICENSE.txt. */
